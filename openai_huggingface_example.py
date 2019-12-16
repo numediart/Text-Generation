c88e7830b3af67d1e665eadcac5e2f87ee575cf6
vdbemden <20713639+vdbemden@users.noreply.github.com>
2019-12-16 14:05:01 +0100

    Update openai_huggingface_example.py

--- openai_huggingface_example.py
+++ openai_huggingface_example.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 # Adapted from https://github.com/huggingface/pytorch-transformers/tree/master/examples#language-generation
-# Copyright UMONS - 2019
+# Copyright 2019 - UMONS
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You should have received a copy of the Apache License Version 2.0 along with this program.
+# If not, see
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import argparse
 import logging
